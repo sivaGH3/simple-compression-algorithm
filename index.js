71bc2982ba7d33cfdc4325be92cb50e8e3172f61
man3in3action <ychnagasivareddy@gmail.com>
2018-12-12 18:06:10 +0530

    fixed rle.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const BWT = require('./bwt');
 
 const readable = fs.createReadStream('./test.txt');
 //const writeFd = fs.openSync('./testCompress.txt','wx');
-//const writeFd = fs.openSync('./testdecomp.txt','wx');
+const writeFd = fs.openSync('./testdecomp.txt','wx');
 
 readable.on('data',(chunk) => {
 	console.log(`recieved ${chunk.length} bytes of data`);
@@ -22,15 +22,15 @@ readable.on('data',(chunk) => {
 	console.log(`decompress string ${decompress[1].length} bytes of data`);
 	const original = BWT.InverseTransform(decompress);
 	console.log(`original string ${original.length} bytes of data`);
-	// console.log(str);
-	// console.log('transform',transform);
-	// console.log('compress',compress);
-	// console.log('decompress',decompress[1]);
-	// console.log('original',original);
+	console.log(str);
+	console.log('transform',transform);
+	console.log('compress',compress);
+	console.log('decompress',decompress[1]);
+	console.log('original',original);
 	console.log(str === original);
-	// fs.writeFile(writeFd,original,() =>{
-	// 	console.log('written succesfully');
-	// });
+	fs.writeFile(writeFd,original,() =>{
+		console.log('written succesfully');
+	});
 })
 
 // split the string on new line char
