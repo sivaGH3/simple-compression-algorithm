948c67a85c53aa2a97b79883077be0f03c2d84f4
man3in3action <ychnagasivareddy@gmail.com>
2018-12-12 19:57:21 +0530

    optimized bwt.js

--- bwt.js
+++ bwt.js
@@ -8,41 +8,50 @@ class BWT {
 		}
 		return 0;
 	}
+	//@TODO: limit str
 	static Transform(str){
 		const len = str.length;
-		let rotations = [];
-		let arr = Array.from(str);
+		const buff = str + str;
+		let sorted_arr_idx = [];
 		for(let i=0;i<len;i++){
-			rotations.push(arr.join(''));
-			//rotation left
-			arr.push(arr.shift());
+			sorted_arr_idx.push(i);
 		}
-		rotations.sort(this._sort);
+		sorted_arr_idx.sort((x,y) =>{
+			for(let i=0;i<len;i++){
+				const diff = buff[x+i].charCodeAt() - buff[y+i].charCodeAt();
+				if(diff !== 0){return diff;}
+			}
+			return 0;
+		})
 		let pos;
-		for(let i=0;i<len;i++){
-			if(rotations[i] === str){pos=i;break;}
-		}
 		let retStr = [];
 		for(let i=0;i<len;i++){
-			retStr.push(rotations[i][len-1]);
+			const ch_id = sorted_arr_idx[i];
+			if(ch_id === 0){
+				pos = i;
+			}
+			retStr.push(buff[ch_id + len -1]);
 		}
-		return [pos,retStr.join('')];
+		return [pos, retStr.join('')];
 	}
 	static InverseTransform([pos,str]){
 		const len = str.length;
-		let arr = Array.from(str);
-		let fin = [];
+		let bwt_arr = Array.from(str);
+		let sorted_arr_idx = [];
 		for(let i=0;i<len;i++){
-			fin.push('');
+			sorted_arr_idx.push(i);
 		}
+		sorted_arr_idx.sort((x,y) =>{
+			const diff = bwt_arr[x].charCodeAt() - bwt_arr[y].charCodeAt();
+			return diff === 0 ? x-y : diff ;
+		});
+		let p = sorted_arr_idx[pos];
+		let retStr = [];
 		for(let i=0;i<len;i++){
-			for(let i=0;i<len;i++){
-				fin[i]=arr[i]+fin[i];
-			}
-			fin.sort(this._sort);
+			retStr.push(bwt_arr[p]);
+			p = sorted_arr_idx[p];
 		}
-		
-		return fin[pos];
+		return retStr.join('');
 	}
 }
 
